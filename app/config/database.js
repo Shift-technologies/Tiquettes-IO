1b97078b127f41dd2b3976bd4c3aa94d2bb45f46
iamtelo <niyio53@gmail.com>
2018-06-05 23:12:08 +0100

    BYE

--- app/config/database.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const mongoose = require('mongoose');
-
-module.exports = function (connectionString) {
-    mongoose.Promise = global.Promise;
-    mongoose.connect(connectionString);
-};
